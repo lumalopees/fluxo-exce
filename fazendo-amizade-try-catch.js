5de58631e6c13145757de72dc53ae45a4d316409
Luma <luuh_lopees@live.com>
2023-10-27 13:35:45 -0300

    :sparkles: 10min | gostei hem, facilita

--- fazendo-amizade-try-catch.js
+++ fazendo-amizade-try-catch.js
@@ -1,17 +1,29 @@
-const checar = () => {
-  if (idade >= 18) {
-    return true; //apta 
-  } else {
-    return false // não apta
+const checar = (idade) => {
+  // if (idade >= 18) {
+  //   return true; //apta 
+  // } else {
+  //   return false // não apta
+  // }
+
+  if (idade < 18) {
+    throw new Error ('Entregada Negada')
   }
 }
 
 const tentar = () => {
   const idade = 17;
 
-  if (checar(idade)) {
-    return 'Entrada Permitida';
-  } else {
-    return 'Entregada Negada';
+  // if (checar(idade)) {
+  //   return 'Entrada Permitida';
+  // } else {
+  //   return 'Entregada Negada';
+  // }
+
+  try { // tenta executar algo
+    checar(idade)
+  } catch (erro) { // captura o erro
+    return erro.message;
   }
 }
+
+console.log(tentar());
