f8166dd96b38836d7c205d4a3f42d34918fd9f08
Luma <luuh_lopees@live.com>
2023-11-13 20:56:39 -0300

    :sparkles: 30min | js js js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 window.onload = () => {
-  const account = document.querySelector('#account')
+  const accountValue = document.querySelector('#account').value;
+  const formAccount = document.querySelector('#transactionForm')
+
+  formAccount.addEventListener('click')
+  if (accountValue.length !== 6) {
+    throw new Error('Necessita de 6 dígitos a conta, por favor verificar')
+  }
 }
