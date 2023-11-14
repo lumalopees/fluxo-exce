6701dc02f9a6c0ef8f202964eea75989e18d7241
Luma <luuh_lopees@live.com>
2023-11-13 22:04:38 -0300

    :sparkles: segundo try catch

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-window.onload = () => {
+const accountCheck = () => {
   const accountValue = document.querySelector('#account').value;
+
+  if (accountValue.length !== 6) {
+    throw new Error('Necessita de 6 dígitos a conta, por favor verificar')
+  } 
+}
+
+const balanceCheck = () => {
+  const amountValue = document.querySelector('#amount').value;
+  const saldoValue = Number(document.querySelector('#saldo').innerText);
+
+
+}
+
+window.onload = () => {
   const formAccount = document.querySelector('#transactionForm')
 
   formAccount.addEventListener('submit', (event) => {
     event.preventDefault();
 
     try {
-      if (accountValue.length !== 6) {
-        throw new Error('Necessita de 6 dígitos a conta, por favor verificar')
-      } 
+      accountCheck();
     } catch (error) {
       alert(erro.message)
     }
